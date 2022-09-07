cc2ee34c1f43e0c9ca0577d30a4282ee44b5945a
SebastianS <eldsebbe@gmail.com>
2022-09-07 11:29:11 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+namn = input("Vad heter du? ")
+
+print("Tjenare", namn)
+age = int(input("Hur gammal är du? "))
+
+print("Så du är", age)
+age = age+5
+
+print("Då är du", age, "om 5 år")
