5f1733c3ab143cf4d0ea5f0f82d07bfd83286e64
SebastianS <eldsebbe@gmail.com>
2022-09-09 09:45:47 +0200

    Gjort uppgift 3.3

--- uppgift3,3.py
+++ uppgift3,3.py
@@ -9,18 +9,18 @@ elevens_poäng=int(input("Hur många poäng har eleven? "))
 
 
 if elevens_poäng <=50 and elevens_poäng >=45:
-    print("A")
+    print("Du har fått ett A")
 
 elif elevens_poäng<45 and elevens_poäng>=40:
-    print("B")
+    print("Du har fått ett B")
 elif elevens_poäng<40 and elevens_poäng>=35:
-    print("C")
+    print("Du har fått ett C")
 elif elevens_poäng<35 and elevens_poäng>=30:
-    print("D")
+    print("Du har fått ett D")
 elif elevens_poäng<30 and elevens_poäng>=25:
-    print("E")
+    print("Du har fått ett E")
 elif elevens_poäng<25:
     print("Du har fått ett F snyggt jobbat")
 
 else:
-    print("Du har fått ett S")
+    print("Du har fått ett Fel!")
