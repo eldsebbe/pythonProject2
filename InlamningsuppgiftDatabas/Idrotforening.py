eaa6f52567514643a19abd7dd18a9d7c269e381a
SebastianS <eldsebbe@gmail.com>
2023-01-06 11:50:01 +0100

    SebastianS version1.0

--- InlamningsuppgiftDatabas/Idrotforening.py
+++ InlamningsuppgiftDatabas/Idrotforening.py
@@ -3,39 +3,45 @@ import Databasen as dbs
 
 sg.theme('DarkAmber')   # Add a touch of color
 # All the stuff inside your window.
-layout1 = [
-    [sg.Text('Förnamn'), sg.InputText()],
-    [sg.Text('Efternamn'), sg.InputText()],
-    [sg.Text('Gatuadress'), sg.InputText()],
-    [sg.Text('postnummer'), sg.InputText()],
-    [sg.Text('Postadress'), sg.InputText()],
-    [sg.Text('Betallat'), sg.InputText()],
-    [sg.Button('lägg till!')]
-]
-layout2 = [
-    [sg.Button("lägg till!"), sg.Button("ta bort"), sg.Button("sök")]
-]
-layout3 = [
-    [sg.Text("Id som ska tas bort:"), sg.InputText()],
-    [sg.Button("ta bort")]
-]
-layout4 = [
-    [sg.Text("Id som ska sökas:"), sg.InputText()],
-    [sg.Button("sök")]
-]
 
-# Create the Window
-window = sg.Window('Menu', layout2)
-window2 = sg.Window('Lägg till', layout1)
-window3= sg.Window("Ta bort", layout3)
-window4= sg.Window("Sök", layout4)
-# Event Loop to process "events" and get the "values" of the inputs
+
+def makewindow(nummer):
+    if nummer==0:
+        layout = [
+            [sg.Button("lägg till!"), sg.Button("ta bort"), sg.Button("sök")]
+        ]
+    elif nummer==1:
+        layout = [
+            [sg.Text('Förnamn'), sg.InputText()],
+            [sg.Text('Efternamn'), sg.InputText()],
+            [sg.Text('Gatuadress'), sg.InputText()],
+            [sg.Text('postnummer'), sg.InputText()],
+            [sg.Text('Postadress'), sg.InputText()],
+            [sg.Text('Betallat'), sg.InputText()],
+            [sg.Button('lägg till!')]
+        ]
+    elif nummer==2:
+        layout = [
+            [sg.Text("Id som ska tas bort:"), sg.InputText()],
+            [sg.Button("ta bort")]
+        ]
+    elif nummer==3:
+        layout = [
+            [sg.Text("Id som ska sökas:"), sg.InputText()],
+            [sg.Text(key="hej")],
+            [sg.Button("sök")]
+        ]
+
+    return sg.Window("window", layout)
 
 dbs.CreateDB()
 dbs.Inserted("Sebbe", "Schaub","Sebbevägen 2", "Jajavägen", "11231", "False")
 dbs.selectuser(1)
 sak=0
 grej=0
+
+
+window=makewindow(0)
 
 while grej==0:
 
@@ -46,19 +52,22 @@ while grej==0:
                 grej=1
                 break
             elif event == "lägg till!":
-                print("Yes")
-                #window.close()
                 sak=1
+                win = makewindow(1)
+                window.close()
+                window2=win
 
             elif event == "ta bort":
-                print("no")
-                #window.close()
                 sak=2
+                win = makewindow(2)
+                window.close()
+                window3 = win
 
             elif event == "sök":
-                print("maybe")
-                #window.close()
                 sak=3
+                win = makewindow(3)
+                window.close()
+                window4 = win
 
 
     elif sak==1:    #lägg till event chane
@@ -66,9 +75,11 @@ while grej==0:
             event2, values2 = window2.read()
             if event2 == sg.WIN_CLOSED:
                 sak=0
+                win = makewindow(0)
+                window2.close()
+                window = win
                 break
             elif event2 == "lägg till!":
-                print('You entered ', values2[0], values2[1], values2[2], values2[3], values2[4], values2[5])
                 dbs.Inserted(values2[0], values2[1], values2[2], values2[3], values2[4], values2[5])
                 sak=1
 
@@ -77,10 +88,12 @@ while grej==0:
             event3, values3 = window3.read()
             if event3 == sg.WIN_CLOSED:
                 sak=0
+                win = makewindow(0)
+                window3.close()
+                window = win
                 break
 
             elif event3 == "ta bort":
-                print('You entered ', values3[0])
                 dbs.deleteuser(values3[0])
                 sak=2
 
@@ -89,14 +102,13 @@ while grej==0:
             event4, values4 = window4.read()
             if event4 == sg.WIN_CLOSED:
                 sak = 0
+                win = makewindow(0)
+                window4.close()
+                window = win
                 break
 
             elif event4 == "sök":
-                print('You entered ', values4[0])
-                print(dbs.selectuser(values4[0]))
+                window4["hej"].Update(dbs.selectuser(values4[0]))
                 sak=3
-
-
-print(dbs.selectuser(2))
 
 window.close()
